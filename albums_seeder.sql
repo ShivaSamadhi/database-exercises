a9948b4e250fae6eae1aae7707a35c6c548e8bcc
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-25 12:22:30 -0500

    sql basic statements part 1 done

---
+++ albums_seeder.sql
@@ -0,0 +1,6 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 50.2, 'pop, post-disco, funk, rock'),
+       ('Whitney Houston', 'The Bodyguard', 1992, 28.7, 'r&b, soul, pop, soundtrack'),
+       ('Bob Marley & The Wailers', 'Legend: The Best of Bob Marley & The Wailers', 1984, 22.9, 'reggae');
