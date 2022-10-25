e55a81f2d962b02a129eef2ee25bac71f7774bf2
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-25 15:09:46 -0500

    sql basic statements part 2 done

--- select_exercises.sql
+++ select_exercises.sql
@@ -1,6 +1,19 @@
 USE codeup_test_db;
 
+SELECT 'Pink Floyd Albums: ' AS 'Info';
 SELECT name FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT 'The Beatles Released ''Sgt. Peppers Lonely Hearts Club Band'' In: ' AS 'Info';
 SELECT release_date FROM albums where name = 'Sgt. Peppers Lonely Hearts Club Band';
+
+SELECT 'The Genre Of ''Nevermind'' By Nirvana Is: ' AS 'Info';
 SELECT genre FROM albums WHERE name = 'Nevermind';
-SELECT name FROM albums WHERE release_date BETWEEN
+
+SELECT 'Albums Released In The 1990s: ' As 'Info';
+SELECT artist, name FROM albums WHERE release_date BETWEEN 1990 AND 1999;
+
+SELECT 'Albums With Less Than 20M Sales: ' As 'Info';
+SELECT artist, name FROM albums WHERE sales < 20;
+
+SELECT 'Albums In The Rock Genre: ' As 'Info';
+SELECT artist, name FROM albums WHERE genre = 'rock';
