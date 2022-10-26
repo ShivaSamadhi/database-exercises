7007348a8285e91890005b68e0aee251ea3a7bef
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-26 09:29:52 -0500

    sql basic statements part 3 done

--- update_exercises.sql
+++ update_exercises.sql
@@ -2,10 +2,12 @@ USE codeup_test_db;
 
 SELECT 'All Albums: ' AS 'Info';
 SELECT name, artist FROM albums;
+UPDATE albums Set sales = (sales * 10);
 
-SELECT
-SELECT
+SELECT 'All Albums Released Before 1980: ' AS 'Info';
+SELECT name, artist, release_date FROM albums WHERE release_date < 1980;
+UPDATE albums Set release_date = (albums.release_date - 100) WHERE release_date < 1980;
 
-SELECT
-SELECT
-
+SELECT 'All Michael Jackson Albums: ' AS 'Info';
+SELECT name, artist FROM albums WHERE artist = 'Michael Jackson';
+UPDATE albums SET artist = 'Peter Jackson' WHERE artist = 'Michael Jackson';
