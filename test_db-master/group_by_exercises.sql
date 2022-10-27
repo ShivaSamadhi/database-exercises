1985a6fde07c3748ccf302300367406024f4ea28
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-27 09:26:27 -0500

    group by exercises

---
+++ test_db-master/group_by_exercises.sql
@@ -0,0 +1,12 @@
+Use employees;
+
+Select Distinct title
+From titles
+Order By title;
+
+Select last_name
+From employees
+Where last_name LIKE 'E%'
+  And last_name LIKE '%E'
+Group By last_name
+Order By last_name;
