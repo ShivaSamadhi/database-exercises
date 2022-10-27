82f1e5e8dcd57fdc5d8844d484448105d6bf8698
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-27 15:21:01 -0500

    join exercises

--- test_db-master/join_exercises.sql
+++ test_db-master/join_exercises.sql
@@ -9,3 +9,12 @@ FROM employees as e
          JOIN departments as d
               ON d.dept_no = dm.dept_no
 WHERE dm.to_date = '9999-01-01' and gender = 'F';
+
+Select T.title, count(DE.emp_no) As total
+From titles As T
+         Join dept_emp as DE
+              On T.emp_no = DE.emp_no
+         Join departments as D
+              On D.dept_no = DE.dept_no
+Where DE.to_date = '9999-01-01' And D.dept_no = 'd009'
+Group By T.title;
