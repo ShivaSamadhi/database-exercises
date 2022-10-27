5533622a46bb59b78f90c0d580b693532b2bb65d
Ramaj Johnson <rjohnson3795@gmail.com>
2022-10-27 09:30:14 -0500

    group by exercises

--- test_db-master/group_by_exercises.sql
+++ test_db-master/group_by_exercises.sql
@@ -10,3 +10,9 @@ Where last_name LIKE 'E%'
   And last_name LIKE '%E'
 Group By last_name
 Order By last_name;
+
+Select last_name
+From employees
+Where last_name Like '%Q%'
+  And last_name Not Like '%qu%'
+Group By last_name;
